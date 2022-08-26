aec0dcc53205c28bc62889cffd707f60ea1335b6
kirankumar290200 <68763255+kirankumar290200@users.noreply.github.com>
2022-08-26 09:51:55 +0530

    Add files via upload

---
+++ UserManager/src/main/resources/UserController.java
@@ -0,0 +1,62 @@
+package com.example.UserManager.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.UserManager.entities.User;
+import com.example.UserManager.services.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+	
+	@GetMapping("/users")
+	public String showUsers(ModelMap model) {
+		
+		
+		logger.info("Getting all Users");
+		Iterable<User> users = userService.GetAllUsers();
+		
+		logger.info("Passing users to view");
+	    model.addAttribute("users", users);    
+		
+        return "users";
+    }
+	
+
+
+}
+
+
+UserExceptionController.java 
+package com.example.UserManager.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.UserManager.exceptions.UserNotFoundException;
+
+@ControllerAdvice
+public class UserExceptionController {
+	@ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Object> exception(UserNotFoundException exception) {
+       return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+    }
+
+}
